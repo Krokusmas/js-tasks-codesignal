c3b21769909fc85315e38d2bb6895cbe3102ba23
Krokusmas <anton.sedchenko@gmail.com>
2020-08-26 02:35:16 +0300

    task 6

--- task 6/js/script.js
+++ task 6/js/script.js
@@ -3,7 +3,6 @@
 // Some people are standing in a row in a park. There are trees between them which cannot be moved. Your task is to rearrange the people by their heights in a non-descending order without moving the trees. People can be very tall!
 
 // Example
-
 // For a = [-1, 150, 190, 170, -1, -1, 160, 180], the output should be
 // sortByHeight(a) = [-1, 150, 160, 170, -1, -1, 180, 190].
 
@@ -17,7 +16,6 @@ function sortByHeight(a) {
   while ((lastIndex = arr.indexOf(-1, lastIndex + 1)) !== -1) {
     treesIndex.push(lastIndex);
   }
-
   function sortNumASC (a, b) {
     if (a > b) return 1;
     if (a < b) return -1;
